c0399fd98256822980e102be71142a9bddbb0420
hannesojala <hannesojala@gmail.com>
2020-05-27 23:06:07 -0700

    Removed code unnecessary to demonstration and added comments

--- PPM.cpp
+++ PPM.cpp
@@ -4,15 +4,21 @@
 using namespace std;
 
 int main() {
+    // Output file
     ofstream image;
-    image.open("output.ppm", 'w');
-    int nx = 200;
-    int ny = 100;
-    image << "P3\n" << nx << " " << ny << "\n255\n"; // PPM header, simple format, specifies pixel data as 3 by 255 (RGB)
+    image.open("output.ppm", std::fstream::out);
+    int nx = 256;
+    int ny = 256;
+
+    // PPM header
+    image << "P3\n" << nx << " " << ny << "\n255\n"; 
+    
+    // Pixel data
     for (int j = ny - 1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
-            image << sin(3.14 * i) * 255.99 << " " << sin(3.14 * j) * 255.99 << " " << 0 << "\n"; // R G B values separated by " "
+            image << i << " " << j << " " << 0 << "\n";
         }
     }
+
     return 0;
 }
